a26d4025552041cdb6604691b8796d5d5876cc1a
OlemcDEV <olemcdev@gmail.com>
2017-04-13 23:41:43 +0200

    Added a register and signup page

--- php/register.php
+++ php/register.php
@@ -3,25 +3,19 @@
 $input_email = $_POST["email"];
 $input_username = $_POST["username"];
 $input_password = $_POST["password"];
-/* Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "root", "", "webquiz");
 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
 //Announce email and username
 echo "Your email is: $input_email and your username is $input_username";
 
 // Attempt insert query execution
-$sql = "INSERT INTO user (email, username, password) VALUES ('$input_email','$input_username','$input_password')";
-if(mysqli_query($link, $sql)){
-    echo "Records added successfully.";
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
+$stmt = $db->prepare("INSERT INTO user (email, username, password) VALUES (:email, :username, :password)");
 
-// Close connection
-mysqli_close($link);
+$stmt->bindValue(':email', $input_email, PDO::PARAM_STR);
+$stmt->bindValue(':username', $input_username, PDO::PARAM_STR);
+$stmt->bindValue(':password', $input_password, PDO::PARAM_STR);
+
+$stmt->execute();
+
+$insertId = $db->lastInsertId();
+
 ?>
--- php/signuplogin.php
+++ php/signuplogin.php
@@ -1,8 +1,14 @@
-
 <?php
-// define variables and set to empty values
+// Define variables and set them to empty values
 $emailErr = $usernameErr = $passwordErr = $genderErr = "";
 $email = $username = $password = $gender = "";
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -20,6 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $usernameErr = "Username is required";
     } else {
         $username = test_input($_POST["username"]);
+
         // check if name only contains letters and whitespace
         if (!(ctype_alnum($username))) {
             $usernameErr = "Only letters and numbers allowed";
@@ -30,6 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $passwordErr = "Password is required";
     } else {
         $password = test_input($_POST["password"]);
+
         if (!ctype_alnum($password)) {
             $passwordErr = "Only letters and numbers allowed!";
         }
@@ -40,74 +48,59 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $gender = test_input($_POST["gender"]);
     }
+
+    // Submit was clicked, try to insert data to database
+    if(isset($_POST["Submit"])) {
+        // Using prepared statement to safely insert user data into tables
+        $stmt = $db->prepare("INSERT INTO user (email, username, password) VALUES (:email, :username, :password)");
+
+        // Declearing types and adding values for the insert
+        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+        $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+        $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+
+        // Execute the query in the database with the inserted values
+        $stmt->execute();
+
+        // Retrieving the id from the insertion if nessesary
+        $insertId = $db->lastInsertId();
+    }
 }
 
-    function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-/* Submit was clicked, try to insert data to database */
-if(isset($_POST["Submit"])) {
-    //connecting
-    $link = mysqli_connect("localhost", "140438", "it", "140438");
-    // Check connection
-    if ($link === false) {
-        die("ERROR: Could not connect. " . mysqli_connect_error());
-        echo "could not connect";
-    }
-    else {
-        echo "connected";
-        //Insert data
-        $sql = "INSERT INTO Weser (email, username, password)
-        VALUES ($email, $username, $password)";
-    }
-}
 ?>
 
 <div class="register">
-    <form id="register" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post"
-            accept-charset="UTF-8">
-        <fieldset >
+    <form id="register" action="/signuplogin" method="post">
+        <fieldset>
             <legend>Register</legend>
             <label for="email" >Email Address*:</label>
             <input type="email" placeholder="Thomas@gmail.com"
-                    name="email" id="email" maxlength="45" value="<?php echo $email;?>"/>
-            <span class="error"> <?php echo $emailErr;?></span>
-            <br><br>
+                    name="email" id="email" maxlength="45" value="<?=$email?>"/>
+            <span class="error"> <?=$emailErr?></span>
+            <br /><br />
 
             <label for="username" >Username*:</label>
             <input type="text" placeholder="Thomas"
-                    name="username" id="username" maxlength="45" value="<?php echo $username;?>"/>
-            <span class="error"> <?php echo $usernameErr;?></span>
-            <br><br>
+                    name="username" id="username" maxlength="45" value="<?=$username?>"/>
+            <span class="error"> <?=$usernameErr?></span>
+            <br /><br />
 
             <label for="password" >Password*:</label>
             <input type="password" placeholder="Thomas123"
-                    name="password" id="password" maxlength="45" value="<?php echo $password;?>"/>
-            <span class="error"> <?php echo $passwordErr;?></span>
-            <br><br>
+                    name="password" id="password" maxlength="45" value="<?=$password?>"/>
+            <span class="error"> <?=$passwordErr?></span>
+            <br /><br />
 
             <label for="gender" >Gender*:</label>
-            <br>
+            <br />
             <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female")
                 echo "checked";?> value="female">Female
             <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male")
                 echo "checked";?> value="male">Male
-            <br>
-            <span class="error"> <?php echo $genderErr;?></span>
-            <br><br>
+            <br />
+            <span class="error"> <?=$genderErr?></span>
+            <br /><br />
             <input type="submit" name="Submit" value="Submit" />
         </fieldset>
     </form>
-    <?php
-    echo "<h2>Your Input:</h2>";
-    echo $email;
-    echo "<br>";
-    echo $username;
-    echo "<br>";
-    echo $gender;
-    echo "<br>";
-    ?>
 </div>
