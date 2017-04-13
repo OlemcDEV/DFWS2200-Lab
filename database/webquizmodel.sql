6bf0a8f4a6e6088d27e6c69e294c3d9c97cd7f19
OlemcDEV <olemcdev@gmail.com>
2017-04-14 00:18:28 +0200

    Added auto increment to user is

--- database/webquizmodel.sql
+++ database/webquizmodel.sql
@@ -21,7 +21,7 @@ USE `webquiz` ;
 -- Table `webquiz`.`user`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `webquiz`.`user` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
