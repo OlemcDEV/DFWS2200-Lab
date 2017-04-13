c4353d96dfa728d9b62b8cdc45608068bf473111
OlemcDEV <olemcdev@gmail.com>
2017-04-13 23:40:37 +0200

    Added a connection file for handling the connect

---
+++ php/connection/connect.php
@@ -0,0 +1,13 @@
+<?php
+
+include "constants.php";
+
+try {
+    $db = new PDO(
+      "mysql:host=$servername;dbname=$dbname", $username, $password);
+} catch (PDOException $ex) {
+    print "ERROR: " . $ex->getMessage() . "<br/>";
+    die();
+}
+
+?>
