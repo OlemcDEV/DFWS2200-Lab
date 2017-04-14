3f2443cdc8c48022d82822ec6b361a681fce08b1
OlemcDEV <olemcdev@gmail.com>
2017-04-14 03:48:14 +0200

    Added a result page

---
+++ php/pages/result.php
@@ -0,0 +1,30 @@
+<?php
+$stmt = $db->prepare("SELECT DISTINCT * FROM quiz WHERE id=:id");
+$stmt->bindValue(":id", $args[1], PDO::PARAM_STR);
+$stmt->execute();
+
+if ($stmt->rowCount() === 0) {
+  echo "Could not find any quiz matching id = $args[1]";
+} else {
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $quiz = $rows[0];
+
+    $stmt = $db->query("SELECT * FROM question WHERE quiz_id=$quiz[id]");
+    $count = $stmt->rowCount();
+
+    // Setting the session vars to init and play the quiz
+    $_SESSION["questions"] = $stmt->fetchAll(); // The questions in correct order
+    $_SESSION["index"] = 0; // The current question
+    $_SESSION["correct"] = array(); // Showing which answer is correct from user
+    ?>
+
+    <h1>Quiz: <?=$quiz["name"]?></h1>
+    <p>Description: <?=$quiz["description"]?></p>
+
+    <div><?="Questions: $count";?><div>
+
+    <h2>Your result:</h2>
+    <p><b>Correct:</b> <?=count($_SESSION["correct"])?> / <?=count($_SESSION["questions"])?> = <?=floor(count($_SESSION["correct"])/count($_SESSION["questions"])*100)?>%</p>
+    <br />
+    <a href="/quiz/<?=$quiz["id"]?>/play"><button>Play again!</button></a>
+<?php } ?>
