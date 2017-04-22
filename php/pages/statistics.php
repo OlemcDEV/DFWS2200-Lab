8d958b5ea22d86d4dd46d537fb743d770b2a3dff
OlemcDEV <olemcdev@gmail.com>
2017-04-22 13:29:10 +0200

    Log of sent in quizzes

--- php/pages/statistics.php
+++ php/pages/statistics.php
@@ -1,17 +1,22 @@
 <?php
-$stmt = $db->query("SELECT *, AVG(correct_answers) AS average FROM result r, quiz q WHERE r.quiz_id=q.id GROUP BY quiz_id ORDER BY average");
+$quizname_query = $db->query("SELECT * FROM result r, quiz q WHERE r.quiz_id=q.id");
+$username_query = $db->query("SELECT * FROM user u, result r WHERE r.user_id=u.id");
 ?>
+<h1>Highscore</h1>
+<h1>Log</h1>
 <table>
     <tr>
-        <th>Name</th>
+        <th>Username</th>
+        <th>Name of quiz</th>
         <th>Results</th>
         <th>Questions</th>
     </tr>
-    <?php while ($row = $stmt->fetch()) { ?>
+    <!--Shows username and quiz name for each result id-->
+    <?php while ( ($col1 = $username_query->fetch()) && ($col2 = $quizname_query->fetch()) ) { ?>
     <tr>
-        <td><a href="/quiz/<?=$row["quiz_id"]?>"><?=$row["name"]?></a></td>
-        <td><?=floor($row["average"]*100 / $row["question_count"])?>%</td>
-        <td><?=$row["question_count"]?></td>
-    </tr>
+        <td><?=$col1["username"]?></td>
+        <td><a href="/quiz/<?=$row["quiz_id"]?>"><?=$col2["name"]?></a></td>
+        <td><?=$col2["correct_answers"]?></td>
+        <td><?=$col2["question_count"]?></td>
     <?php } ?>
 </table>
